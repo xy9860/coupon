46824c07386428efcdc850252fba51ea2265245b
xy9860 <luoxin@yohitrip.com>
2021-01-04 11:39:04 +0800

    初始化

---
+++ src/test/java/com/xy9860/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xy9860.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
