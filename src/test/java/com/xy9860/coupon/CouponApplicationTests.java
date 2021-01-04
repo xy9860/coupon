881915d72d51f3d74cb11fa56350bf36a9b001be
xy9860 <luoxin@yohitrip.com>
2021-01-04 11:44:02 +0800

    初始化

--- src/test/java/com/xy9860/coupon/CouponApplicationTests.java
+++ src/test/java/com/xy9860/coupon/CouponApplicationTests.java
@@ -3,7 +3,7 @@ package com.xy9860.coupon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CouponApplicationTests {
 
     @Test
